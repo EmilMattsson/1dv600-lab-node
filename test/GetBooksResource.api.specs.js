a0af0c715a167b7c88ac4aefc8ef727291074081
EMIL-DATOR\Emil <emil.emanuel@hotmail.com>
2018-03-02 15:53:28 +0100

    refactor

--- test/GetBooksResource.api.specs.js
+++ test/GetBooksResource.api.specs.js
@@ -1,17 +1,20 @@
 var request = require('supertest')
 var app = require('../app')
+var getBooks = require('../app/resources/GetBooksResource')
 
 describe('Get Books Resource', () => {
 
     describe('GET a list of books', () => {
+        var bookList
+        getBooks((data) => {
+            bookList = data
+        })
 
         it('respond with list', (done) => {
-
             request(app)
                 .get('/api/books')
                 .set('Accept', 'application/json')
-                .expect('Content-Type', /json/)
-                .expect(200, done)
+                .expect(200, bookList, done)
         })
     })
 })
